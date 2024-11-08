2604795b5d0a4dd5d37e8fef51e0b73c2d131de0
AlexDo1 <alexander.dolich@kit.edu>
2024-11-08 18:42:48 +0100

    refactor aggregate method to support multiple aggregation methods

--- stgrid2area/area.py
+++ stgrid2area/area.py
@@ -133,24 +133,33 @@ class Area():
         
         return clipped
     
-    def aggregate(self, stgrid: xr.DataArray, operations: list[str], save_result: bool = False, skip_exist: bool = False) -> pd.DataFrame:
+    def aggregate(self, stgrid: xr.Dataset, variable: str, method: str, operations: list[str], save_result: bool = False, skip_exist: bool = False) -> pd.DataFrame:
         """
-        Aggregate the spatiotemporal grid to the area's geometry.  
-        Usually, you first perform the `clip` and then aggregate the clipped stgrid. Using the clipped  
+        Wrapper function to aggregate the spatiotemporal grid to the area's geometry using either the exact_extract or xarray method.  
+        Usually, you first perform the `clip` and then aggregate the clipped stgrid. Using the clipped
         raster data also results in much faster aggregation.  
-        The aggregation is spatially (e.g. the spatial mean), so the time dimension is preserved and 
-        the result is a time series DataFrame with the same time dimension as the input grid.
+        The aggregation is spatially (e.g. the spatial mean), so the time dimension is preserved and
+        the result is a time series DataFrame with the same time dimension as the input grid.  
+        Depending on the method, the aggregation is done using exact_extract or xarray. The exact_extract
+        method yields weighted spatial statistics based on the fraction of the pixel that is covered by the geometry,
+        while the xarray method yields unweighted spatial statistics based on the pixel values. As weighted
+        statistics are only possible with spatial dimensions >= (2, 2), the exact_extract method will raise an error
+        if the spatial dimensions are 1 in at least one direction. In this case, the xarray method can be used as a fallback.
 
         Parameters
         ----------
-        stgrid : xr.DataArray
-            The spatiotemporal grid to aggregate. Must be a xr.DataArray, as only one variable  
+        stgrid : xr.Dataset
+            The spatiotemporal grid to aggregate. Must be a xr.Dataset, as only one variable
             can be aggregated.
+        variable : str
+            The variable in stgrid to aggregate.
+        method : str, optional
+            The method to use for aggregation.  
+            Can be "exact_extract", "xarray".
         operations : list[str]
-            The operations to use for aggregation.  
+            The operations to use for aggregation.
             Can be "mean", "min", "median", "max", "stdev", "quantile(q=0.XX)" and all other operations that are 
             supported by the [exact_extract](https://github.com/isciences/exactextract) package.
-            The default is "mean".
         save_result : bool, optional
             If True, the aggregated timeseries will be saved to the output directory of the area.  
             The default is False.
@@ -169,18 +178,65 @@ class Area():
         if skip_exist and self.has_aggregate:
             return pd.read_csv(self.output_path / f"{self.id}_aggregated.csv", index_col="time")
         
-        # Check dimensionality of gridded data, calculating weighted statistics with exactaxtract can only be done if shape is >= (2, 2)
-        if 1 in stgrid.isel(time=0).shape:
-            raise NotImplementedError(f"{self.id}: Gridded data has spatial dimensionality of 1 in at least one direction, aggregation for 1-D data is not supported at the moment.")
-
-        # Check if the stgrid is a xarray DataArray
-        if not isinstance(stgrid, xr.DataArray):
-            raise TypeError(f"{self.id}: The stgrid must be a xarray DataArray.")
+        # Check if the stgrid is a xarray Dataset
+        if not isinstance(stgrid, xr.Dataset):
+            raise TypeError(f"{self.id}: The stgrid must be a xarray Dataset.")
         
+        # Select the variable in the stgrid
+        stgrid = stgrid[variable]
+
         # Check if operations is a list
         if not isinstance(operations, list):
             operations = [operations]
 
+        # Load the clipped grid, as this yielded the fastest computation times
+        stgrid = stgrid.load()
+
+        if method == "exact_extract":
+            return self._aggregate_exact_extract(stgrid, variable, operations, save_result, skip_exist)
+        elif method == "xarray":
+            return self._aggregate_xarray(stgrid, variable, operations, save_result, skip_exist)
+        else:
+            raise ValueError(f"{self.id}: The method {method} is not supported. Use 'exact_extract', 'xarray'.")
+    
+    def _aggregate_exact_extract(self, stgrid: xr.DataArray, variable: str, operations: list[str], save_result: bool = False, skip_exist: bool = False) -> pd.DataFrame:
+        """
+        Aggregate the spatiotemporal grid to the area's geometry.  
+        Usually, you first perform the `clip` and then aggregate the clipped stgrid. Using the clipped  
+        raster data also results in much faster aggregation.  
+        The aggregation is spatially (e.g. the spatial mean), so the time dimension is preserved and 
+        the result is a time series DataFrame with the same time dimension as the input grid.  
+        This method uses the exact_extract package for the aggregation, which yields weighted spatial
+        statistics based on the fraction of the pixel that is covered by the geometry.
+
+        Parameters
+        ----------
+        stgrid : xr.DataArray
+            The spatiotemporal grid to aggregate. Must be a xr.DataArray with the variable to aggregate.
+        variable : str
+            The variable in stgrid to aggregate.
+        operations : list[str]
+            The operations to use for aggregation.  
+            Can be "mean", "min", "median", "max", "stdev", "quantile(q=0.XX)" and all other operations that are 
+            supported by the [exact_extract](https://github.com/isciences/exactextract) package.
+        save_result : bool, optional
+            If True, the aggregated timeseries will be saved to the output directory of the area.  
+            The default is False.
+        skip_exist : bool, optional
+            If True, the aggregation will be skipped if the aggregated timeseries already exists.  
+            In this case, the existing timeseries grid will be returned.  
+            The default is False.
+
+        Returns
+        -------
+        pd.DataFrame
+            The aggregated spatiotemporal grid.
+
+        """
+        # Check dimensionality of gridded data, calculating weighted statistics with exactaxtract can only be done if shape is >= (2, 2)
+        if 1 in stgrid.isel(time=0).shape:
+            raise ValueError(f"{self.id}: Gridded data has spatial dimensionality of 1 in at least one direction, aggregation for 1-D data is not possible with the exact_extract method, you can use area.aggregate_xarray() instead.")
+        
         # Set the crs of the geometry to the crs of the stgrid
         geometry = self.geometry.to_crs(stgrid.rio.crs.to_string())
 
@@ -199,7 +255,7 @@ class Area():
         # Set the index to the time values and rename the columns
         for i, df in enumerate(sliced_dfs):
             df.index = time_index
-            df.columns = [f"{stgrid.name}_{operations[i]}"]
+            df.columns = [f"{variable}_{operations[i]}"]
         
             # Replace quantile column names to not include brackets, equal sign and points
             for col in df.columns:
@@ -208,10 +264,95 @@ class Area():
                     q = int(float(col.split('=')[1].split(')')[0]) * 100)
 
                     # replace the column name
-                    df.rename(columns={col: f"{stgrid.name}_quantile{q}"}, inplace=True)                
+                    df.rename(columns={col: f"{variable}_quantile{q}"}, inplace=True)                
 
         # Concatenate the dataframes
         df_timeseries = pd.concat(sliced_dfs, axis=1)
+
+        # Label the index
+        df_timeseries.index.name = "time"
+
+        # Save the aggregated grid to the output directory of the area
+        if save_result:
+             # Create the output directory if it does not exist
+            self.output_path.mkdir(parents=True, exist_ok=True)
+
+            # Save the aggregated timeseries to the output directory
+            df_timeseries.to_csv(self.output_path / f"{self.id}_aggregated.csv", index_label="time")
+        
+        return df_timeseries
+    
+    def _aggregate_xarray(self, stgrid: xr.DataArray, variable: str, operations: list[str], save_result: bool = False, skip_exist: bool = False) -> pd.DataFrame:
+        """
+        Aggregate the spatiotemporal grid to the area's geometry.  
+        Usually, you first perform the `clip` and then aggregate the clipped stgrid. Using the clipped  
+        raster data also results in much faster aggregation.  
+        The aggregation is spatially (e.g. the spatial mean), so the time dimension is preserved and 
+        the result is a time series DataFrame with the same time dimension as the input grid.  
+        This method uses xarray directly for the aggregation, which yields unweighted spatial
+        statistics based on the pixel values.
+
+        Parameters
+        ----------
+        stgrid : xr.DataArray
+            The spatiotemporal grid to aggregate. Must be a xr.DataArray, as only one variable  
+            can be aggregated.
+        variable : str
+            The variable in stgrid to aggregate.
+        operations : list[str]
+            The operations to use for aggregation.  
+            Can be "mean", "min", "median", "max", "stdev", "quantile(q=0.XX)" and all other operations that are 
+            supported by the [exact_extract](https://github.com/isciences/exactextract) package.
+        save_result : bool, optional
+            If True, the aggregated timeseries will be saved to the output directory of the area.  
+            The default is False.
+        skip_exist : bool, optional
+            If True, the aggregation will be skipped if the aggregated timeseries already exists.  
+            In this case, the existing timeseries grid will be returned.  
+            The default is False.
+
+        Returns
+        -------
+        pd.DataFrame
+            The aggregated spatiotemporal grid.
+
+        """
+        # Infer the spatial dimensions of the grid based on common dimension names TODO: improve this / spatial dims as input?
+        spatial_dims = [dim for dim in stgrid.dims if dim in ["latitude", "longitude", "lat", "lon", "x", "y", "X", "Y"]]        
+
+        # Define the computations
+        for operation in operations:
+            if operation == "mean":
+                result = stgrid.mean(dim=spatial_dims)
+            elif operation == "min":
+                result = stgrid.min(dim=spatial_dims)
+            elif operation == "median":
+                result = stgrid.median(dim=spatial_dims)
+            elif operation == "max":
+                result = stgrid.max(dim=spatial_dims)
+            elif operation == "stdev":
+                result = stgrid.std(dim=spatial_dims)
+            elif "quantile" in operation:
+                q = float(operation.split("=")[1].split(")")[0])
+                result = stgrid.quantile(q, dim=spatial_dims)
+            else:
+                raise ValueError(f"{self.id}: The operation {operation} is not supported.")
+            
+            # Compute the result
+            result = result.compute()
+            
+            # Convert the result to a DataFrame
+            df = result.to_dataframe()[[variable]]
+            df.columns = [f"{variable}_{operation}" if not "quantile" in operation else f"{variable}_quantile{int(q*100)}"]
+
+            # Concatenate the dataframes
+            if operation == operations[0]:
+                df_timeseries = df
+            else:
+                df_timeseries = pd.concat([df_timeseries, df], axis=1)
+
+        # Label the index
+        df_timeseries.index.name = "time"
 
         # Save the aggregated grid to the output directory of the area
         if save_result:
