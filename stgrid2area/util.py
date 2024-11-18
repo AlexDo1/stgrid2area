e73c3db509e48cfe269c6c711b995c116489f063
AlexDo1 <alexander.dolich@kit.edu>
2024-11-18 16:27:41 +0100

    Add optional sorting by proximity in geodataframe_to_areas function

--- stgrid2area/util.py
+++ stgrid2area/util.py
@@ -3,7 +3,7 @@ from .area import Area
 import geopandas as gpd
 
 
-def geodataframe_to_areas(areas: gpd.GeoDataFrame, id_column: str, output_dir: str) -> list[Area]:
+def geodataframe_to_areas(areas: gpd.GeoDataFrame, id_column: str, output_dir: str, sort_by_proximity: bool = False) -> list[Area]:
     """
     Convert a GeoDataFrame of areas to a list of Area objects to be used as input for the ParallelProcessor.
 
@@ -11,9 +11,17 @@ def geodataframe_to_areas(areas: gpd.GeoDataFrame, id_column: str, output_dir: s
     ----------
     areas : gpd.GeoDataFrame
         The GeoDataFrame of areas.
+    id_column : str
+        The name of the column in the GeoDataFrame that contains the unique identifier for each area.
     output_dir : str
         The output directory where results will be saved.  
         Will always be a subdirectory of this directory, named after the area's id.
+    sort_by_proximity : bool, optional
+        Whether to sort the areas by proximity. 
+        Default is False.
+        This is especially useful when using the ParallelProcessor with batches of areas, as this makes
+        sure that batched areas are close to each other, which is more efficient, as a smaller portion of the 
+        stgrid will be loaded into memory.
 
     Returns
     -------
@@ -21,6 +29,10 @@ def geodataframe_to_areas(areas: gpd.GeoDataFrame, id_column: str, output_dir: s
         The list of Area objects.
 
     """
+    # Sort the areas by proximity
+    if sort_by_proximity:
+        areas = areas.sort_values(by="geometry")
+
     areas_list = []
 
     for idx in areas.index:
